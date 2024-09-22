c7fea2b863934e5d9dac70095aa5e7d32d559a8d
i2z1 <i2z1@yandex.ru>
2024-09-22 15:53:26 +0300

    add test for non exist data

--- tests/testthat/cbr_currency.R
+++ tests/testthat/cbr_currency.R
@@ -2,4 +2,9 @@ library(testthat)
 
 test_that("cbr_currency() works", expect_equal({
   ncol(cbr_currency(currency = "R01239", from = "2024-09-01", to = "2024-09-10"))
-}, {4}))
+}, 4))
+
+# not working days
+test_that("cbr_currency() works", expect_equal({
+  ncol(cbr_currency(currency = "R01239", from = "2024-01-01", to = "2024-01-02"))
+}, 4))
